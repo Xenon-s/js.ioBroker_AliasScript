a7d0c770c58b6320145210c9d55bbd8a97964599
steffen <ente_s@hotmail.de>
2021-05-20 11:24:58 +0200

    initial commit V 0.1

--- README.md
+++ README.md
@@ -1,1 +1,81 @@
-js.ioBroker_AliasScript
+
+
+# Alias Script fuer ioBroker V0.1
+Dieses Script ermöglicht es, alias States zu erzeugen. Die Unterschied zu schon vorhandenen Boardmitteln, bzw. Adaptern ist, dass man nahezu nichts von Hand eingeben muss. Es reicht der Name des Datenpunktes.
+Das Script erstellt dann anhand des device-Namens eine neue Ordnerstruktur mit ausschließlich den Datenpunkten, die vom User vorgegeben werden. 
+
+## Was sollte beachtet werden?
+Aktuell funktioniert dieses Script nur mit dem ioBroker Zigbee Adapter. Erweiterungen werden folgen (können auch per Issue angefragt werden). Es sollten nur die wirklich relevanten Datenpunkte per Alias angelegt werden, um keine unnötige Systemlast zu erzeugen.
+
+## Was ist geplant?
+- Delete Button: Per Klick sollen alle nicht mehr vorhandenen Datenpunkte automatisch entfernt werden
+- Integration weiterer Adapter
+
+## Wie funktioniert es?
+Es gibt im Script selber nur zwei Parameter, die der User ändern kann.
+- den neuen Zielpfad
+- die Zigbee Instanz (aktuell nur Zigbee möglich!)
+
+# Anleitung
+## Script erstellen
+Ein neues JS Script in iobroker erstellen und das Script aus "script.js" kopieren und einfügen. <br>
+
+![erstellung_1.png](/pictures/erstellung_1.png) <br> 
+![erstellung_2.png](/pictures/erstellung_2.png) <br>
+
+## Einstellungen
+### Script
+Folgendes kann optional geändert werden:
+- Zielpfad (Standard: "0_userdata.0.alias_zigbee")
+- Adapterinstanz (Standard: "zigbee.0")
+
+![User_Input.png](/pictures/User_Input.png) <br>
+
+### Datenpunkte
+Es wird der Datenpunkt 0_Config zur Verfügung gestellt. Im Datenpunkt "attributes" werden dann die gewüschten States eingetragen. Standardmäßig stehen hier bereits "state, temperature, occupancy" drin. Jeder weitere Eintrag muss mit einem Komma getrennt werden. 
+
+![attributes.png](/pictures/attributes.png) <br>
+
+### Sonstiges
+Original Struktur aus dem Adapter:
+![original.png](/pictures/original.png) <br>
+
+Struktur durch das Skript:
+![alias.png](/pictures/alias.png) <br>
+
+
+
+**Falls euch meine Arbeit gefällt :** <br>
+
+[![Paypal Donation](https://img.shields.io/badge/paypal-donate%20%7C%20spenden-blue.svg)](https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3EYML5A4EMJCW&source=url)
+
+
+## Changelog
+
+### 0.1 (20-05.2021)
+* (xenon-s) initial commit
+
+
+# License
+
+The MIT License (MIT)
+
+Copyright (c) 2021 xenon-s
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ script.js
@@ -0,0 +1,235 @@
+// script um Zigbee states zu spiegeln und unzubennen
+const zigbee = 'zigbee.0';
+const target = '0_userdata.0.alias_zigbee'
+
+// Finger weg !
+
+class aliasZigbee {
+    constructor() {
+        this.message = `zigbee alias script: Version 0.1, dev: xenon-s Have fun with it!`;
+        this.dev = 'xenon-s'
+        this.objName = []; // Namen aller Zigbee Devices
+        this.arrayTrigger = []; // Array mit allen Trigger Pfaden
+        this.ids = [];  // Array aller Zigbee Ids aus dem Adapter
+        this.objNew = {};   // Objekt mit den neu zusammengebauten Objekten, die unter userdata angelegt werden
+        this.config = {};
+        this.attributes = [];
+        log(this.message);
+        loop('start');
+    };
+};
+
+new aliasZigbee();
+
+// get ids from zigbee
+/**
+* @param {string} cmd
+*/
+async function loop(cmd) {
+
+    if (cmd === 'start') {
+        await createConfig();
+    };
+    this.ids = Array.prototype.slice.apply($(`state[id='${zigbee}*']`));
+    this.objName = await getIDs();
+    this.objNew = await getName();
+    this.arrayTrigger = await createDp();
+    await createTrigger(cmd);
+};
+
+async function createConfig() {
+    this.config = {
+        // refresh_Button: {
+        //     parse: {
+        //         "name": "Refresh Button",
+        //         "type": "boolean",
+        //         "read": false,
+        //         "write": true,
+        //         "role": "button"
+        //     },
+        // },
+        attributes: {
+            parse: {
+                "name": "Zigbee State Types",
+                "type": "string",
+                "read": true,
+                "write": true,
+                "role": "state"
+            },
+            ini: ['state', 'temperature', 'occupancy'],
+        }
+    };
+
+    // Config Dps erstellen
+    for (const i in this.config) {
+        let path = '';
+        path = `${target}.0_Config.${i}`;
+        let com = '';
+        com = JSON.stringify(this.config[i].parse);
+
+        // Pruefen ob DPs vorhanden
+        if (!getObject(path)) {
+            createStateAsync(path, JSON.parse(com), async () => {
+                if (i == 'attributes') {
+                    setStateAsync(path, this.config[i].ini, true)
+                };
+            });
+        } else {
+            if (i == 'attributes') {
+                const result = await getStateAsync(path);
+                this.attributes = result.val.split(',');
+            };
+        };
+
+        // // trigger auf config
+        on({ id: path, change: "any", ack: false }, async obj => {
+            if (obj.id === `${target}.0_Config.attributes`) {
+                this.attributes = obj.state.val.split(',');
+                console.warn(JSON.stringify(this.attributes))
+                const value = obj.state.val;
+                setState(path, value, true);
+            };
+            loop(null);
+        });
+
+    };
+};
+
+// eindeutige device namen aus der id holen
+async function getIDs() {
+    let arrTemp = [];
+    this.ids.forEach(async data => {
+        let str = ``;
+        str = data;
+        let name = ``
+        let res = str.lastIndexOf('.')
+        name = str.slice(0, res);
+        // namen bereits im array enthalten?
+        // wenn nicht, hinzufügen
+        if (!arrTemp.includes(name)) {
+            arrTemp.push(name)
+        };
+    });
+    return arrTemp;
+};
+
+// neues Objekt bauen 
+async function getName() {
+    let objId = {};
+    for (const i in this.objName) {
+        const data = this.objName[i];
+        const id = data;
+        let objTempName = {};
+        let objOriTemp = {};
+
+        // namen aus dem device auslesen
+        objTempName = await getObjectAsync(data); // objekt auslesen
+
+        for (const name in this.attributes) {
+            let strTemp = ``;
+            // 
+            if (objTempName.type == 'device') {
+                strTemp = `${data}.${this.attributes[name]}`;
+
+                // nach id suchen und prüfen ob 'angegebene attribute' enthalten sind
+                if (this.ids.includes(strTemp)) {
+                    objOriTemp = await getObjectAsync(strTemp); // objekt auslesen
+                    if (objOriTemp.type == 'state') {
+
+                        // objekt neu schreiben und vom User ausgewaehlte DPs speichern
+
+                        if (objOriTemp.common.name === 'On/off state of the switch' || objOriTemp.common.name === 'Switch state') {
+                            objOriTemp.common.name = 'Switch on/off';
+                        };
+
+                        const deviceName = `${objOriTemp.common.name} ${objTempName.common.name}`
+
+                        objId[`${id}.${this.attributes[name]}`] = {
+                            id: id,
+                            name: objTempName.common.name.replace(/ /g, '_'),
+                            nameNew: deviceName,
+                            idState: objOriTemp._id,
+                            common: objOriTemp.common
+                        };
+                    };
+                };
+            };
+        };
+    };
+    return objId;
+};
+
+// datenpunkte erstellen
+async function createDp() {
+    let arrTemp = [];
+    for (const i in this.objNew) {
+        let pathNew = ``;
+        let com = ``;
+
+        pathNew = `${target}.${this.objNew[i].name}.${this.objNew[i].common.name.replace(/ /g, '_')}`;
+
+        this.objNew[i].common.name = this.objNew[i].nameNew;
+
+        com = JSON.stringify(this.objNew[i].common);
+
+        // neue Datenpunkte erzeugen
+        if (!getObject(pathNew)) {
+            createStateAsync(pathNew, JSON.parse(com), async () => {
+                const value = await getStateAsync(this.objNew[i].idState)
+                if (value != null) {
+                    await setStateAsync(pathNew, value.val, true)
+                };
+            });
+        };
+
+        const objTrigger = { triggerAdapter: this.objNew[i].idState, triggerCopy: pathNew }
+        arrTemp.push(objTrigger);
+    };
+    return arrTemp;
+};
+
+// trigger erstellen
+/**
+* @param {string} cmd
+*/
+async function createTrigger(cmd) {
+    for (const i in this.arrayTrigger) {
+
+        let triggerAdapter = '';
+        let triggerCopy = '';
+
+        triggerAdapter = this.arrayTrigger[i].triggerAdapter;
+        triggerCopy = this.arrayTrigger[i].triggerCopy;
+
+        // "subscriptions" löschen
+        if (cmd !== 'start') {
+            if (unsubscribe(triggerAdapter)) {
+            };
+
+            if (unsubscribe(triggerCopy)) {
+            };
+        };
+
+        // trigger auf adapter.state
+        on({ id: triggerAdapter, change: "any", ack: true }, async obj => {
+            const value = obj.state.val;
+            setState(triggerCopy, value, true);
+        });
+
+        // trigger auf copy.state
+        on({ id: triggerCopy, change: "any", ack: false }, async obj => {
+            const value = obj.state.val;
+            setState(triggerAdapter, value);
+        });
+    };
+};
+
+
+
+
+
+
+
+
+
+
